9d005fafa17f8d6bdb9bb59ad43ebc57c7e5a479
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-02-25 16:52:39 +0700

    'test'

---
+++ comment.py
@@ -0,0 +1,12 @@
+# membuat commentar seperti ini
+
+# hastag pagar, digunakan untuk satu statment
+
+# jika ingin multilien statement bisa menggunkan ...
+
+'''
+statment pertama
+statment kedua
+statment ketiga
+'''
+print('gunakan tanda petik tiga untuk membuat commmentaar multi line');
